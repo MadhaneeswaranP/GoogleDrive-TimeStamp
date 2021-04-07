1cc55292454dc10ced713b9ce92f5f4e83b5753b
MadhaneeswaranP <78028332+MadhaneeswaranP@users.noreply.github.com>
2021-04-07 20:31:01 +0530

    Update currenttime.js

--- currenttime.js
+++ currenttime.js
@@ -14,11 +14,6 @@ fs.readFile("time.txt",`utf8`,function(err,data){
 });
 var a;
 
-fs.readdir("./",function(err,data){
-    if(err) throw err;
-    console.log(data);
-    
-})
 
 const express = require("express");
 
